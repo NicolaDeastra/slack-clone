645d8062650a57939f44cd5b41b81b69cc311c97
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-08-21 19:24:43 +0700

    fetch data and styling chat commponent

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,29 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 
+import db from "../../firebase";
+import Message from "../Message/Message";
+
 import "./Chat.css";
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+  const [roomMessages, setRoomMessages] = useState([]);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+    }
+
+    db.collection("rooms")
+      .doc(roomId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, [roomId]);
 
   return (
     <div className="chat">
-      <h2>You are in the {roomId} room</h2>
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong># general</strong>
+            <strong>#{roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
         <div className="chat__headerRight">
           <p>
-            <InfoOutlinedIcon /> Details
+            <InfoOutlinedIcon />
           </p>
         </div>
+      </div>
+      <div>
+        {roomMessages.map(({ message, timestamp, user, userImage }) => (
+          <Message
+            message={message}
+            timestamp={timestamp}
+            user={user}
+            userImage={userImage}
+          />
+        ))}
       </div>
     </div>
   );
